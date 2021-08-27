c5043dc6de5d698d9d606f63b6835592646a1f65
rex2go <rexrex231@gmail.com>
2021-08-27 18:47:33 +0200

    fix

--- src/main/java/eu/rex2go/chat2go/filter/BadWordFilter.java
+++ src/main/java/eu/rex2go/chat2go/filter/BadWordFilter.java
@@ -2,17 +2,19 @@ package eu.rex2go.chat2go.filter;
 
 import eu.rex2go.chat2go.Chat2Go;
 import eu.rex2go.chat2go.config.FilterConfig;
-import lombok.Getter;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class BadWordFilter extends Filter {
 
-    @Getter
-    private static final List<String> badWords = FilterConfig.getBlockedWords();
+    public static List<String> getBadWords() {
+        return FilterConfig.getBlockedWords();
+    }
 
     public static boolean addBadWord(String badWord) {
+        List<String> badWords = FilterConfig.getBlockedWords();
+
         if (badWords.contains(badWord)) return false;
 
         badWords.add(badWord);
@@ -23,6 +25,8 @@ public class BadWordFilter extends Filter {
     }
 
     public static boolean removeBadWord(String badWord) {
+        List<String> badWords = FilterConfig.getBlockedWords();
+
         if (!badWords.contains(badWord)) return false;
 
         badWords.remove(badWord);
@@ -34,6 +38,8 @@ public class BadWordFilter extends Filter {
 
     @Override
     public List<String> filter(String message) {
+        List<String> badWords = FilterConfig.getBlockedWords();
+
         ArrayList<String> list = new ArrayList<>();
 
         for (String badWord : badWords) {
